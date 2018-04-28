3e648bae221af1c1c091a7026f08d7ba484d35ae
wzieba <wojciech.zieba@protonmail.com>
2018-04-28 16:19:14 +0200

    Addings Travis CI config, moving to PostgreSQL database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_04_26_211120) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quotes", force: :cascade do |t|
     t.string "text", null: false
     t.datetime "created_at", null: false
